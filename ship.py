97be9106435138c572ec9006cc776a8ab4305df4
Bencki <michaldebski95@gmail.com>
2021-10-13 20:12:21 +0200

    New ship parameters

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
         """Inicjalizacja statku kosmicznego i jego położenie początkowe"""
 
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #Wczytywanie obrazu statku kosmicznego
@@ -15,6 +16,18 @@ class Ship:
 
         #Każdy nowy statek kosmiczny pojawiasie na dole ekranu
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
     def blitme(self):
         #Wyświetlanie statku kosmicznego w jego aktualnym położeniu
